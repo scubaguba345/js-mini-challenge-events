bf16802d0dc5843202ca5c572027aef59d27aedf
scubaguba345 <brandon.gubitosa@gmail.com>
2020-08-04 17:36:05 -0400

    done

--- index.js
+++ index.js
@@ -32,15 +32,38 @@ PLAYERS.forEach(renderPlayer)
 
 
 /***** Deliverable 1 *****/
+const header = document.getElementById('header')
 function toggleColor(element) {
-  if (element.style.color === "red") {
-    element.style.color = "black"
-  } else {
-    element.style.color = "red"
+    if (element.style.color === "red") {
+      element.style.color = "black"
+    } else {
+      element.style.color = "red"
+    }
   }
-}
 
+  header.addEventListener('click', function(e) {
+    toggleColor(e.target);
+  })
 
 /***** Deliverable 2 *****/
 
+const newForm = document.getElementById("new-player-form")
+newForm.addEventListener('submit', function(e) {
+  e.preventDefault()
+  //console.log(e.target)
+  let newPlayer = {};
+  newPlayer.number = newForm[0].value
+  newPlayer.name = newForm[1].value
+  newPlayer.nickname = newForm[2].value
+  newPlayer.photo = newForm[3].value
+  newPlayer.likes = 1;
+  renderPlayer(newPlayer)
+})
+
 /***** Deliverable 3 *****/
+document.addEventListener('click', function(e) {
+  if(e.target.className === "like-button") {
+    let likes = e.target.parentNode.querySelector('.likes') //we are going one up from the target of like button to grab the number of likes above
+    likes.innerText = `${(parseInt(likes.innerText) + 1)} likes`
+  }
+})
